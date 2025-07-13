6571d2c974a7fb0508169d6ef204b41141a80a9b
finn <bishoe01@gmail.com>
2025-07-13 20:11:13 +0900

    ✨ [feat] #5 카메라 기능 및 상태 관리 ViewModel 구현

--- Chalkak/Presentation/Camera/CameraViewModel.swift
+++ Chalkak/Presentation/Camera/CameraViewModel.swift
@@ -5,17 +5,61 @@
 //  Created by 배현진 on 7/12/25.
 //
 
+import AVFoundation
 import Foundation
+import Photos
 import SwiftData
+import SwiftUI
 
-final class CameraViewModel: ObservableObject {
+class CameraViewModel: ObservableObject {
     private var modelContext: ModelContext?
-
-    init(context: ModelContext?) {
-        self.modelContext = context
+    private let model: CameraManager
+    private let session: AVCaptureSession
+    let cameraPreview: AnyView
+    
+    @Published var isFlashOn = false
+    @Published var isSilentModeOn = false
+    @Published var cameraPostion: AVCaptureDevice.Position = .back
+    @Published var isRecording = false
+    
+    func configure() {
+        model.requestAndCheckPermissions()
     }
     
+    func switchFlash() {
+        isFlashOn.toggle()
+    }
+    
+    func switchSilent() {
+        isSilentModeOn.toggle()
+    }
+
+    func startVideoRecording() {
+        model.startRecording()
+        isRecording = true
+    }
+    
+    func stopVideoRecording() {
+        model.stopRecording()
+        isRecording = false
+    }
+    
+    func changeCamera() {
+        cameraPostion = cameraPostion == .back ? .front : .back
+        model.switchCamera(to: cameraPostion)
+    }
+    
+    init(context: ModelContext?) {
+        model = CameraManager()
+        session = model.session
+        cameraPreview = AnyView(CameraPreviewView(session: session))
+        modelContext = context
+        
+        model.$isRecording
+            .assign(to: &$isRecording)
+    }
+
     func updateContext(_ context: ModelContext) {
-        self.modelContext = context
+        modelContext = context
     }
 }
