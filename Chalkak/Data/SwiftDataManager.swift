dd58373c66f98798ca335f17863f074ef96ba099
pearhyunjin <pearhyunjin@gmail.com>
2025-07-15 21:34:58 +0900

    ✅ [chore] #26 SwiftDataManager DooC 작성

--- Chalkak/Data/SwiftDataManager.swift
+++ Chalkak/Data/SwiftDataManager.swift
@@ -9,6 +9,45 @@ import Foundation
 import SwiftData
 import SwiftUI
 
+/**
+ SwiftData 사용을 편리하게 하기 위한 클래스
+ 
+ `SwiftDataManager`는 SwiftData 사용을 편리하게 하기 위해 쿼리 메서드를 모아 관리합니다.
+ 
+ ## 사용 예시
+ ```
+ @Published var clips: [Clip] = []
+ 
+ func loadClips() {
+     clips = SwiftDataManager.shared.fetchAllClips()
+ }
+ 
+ func createClip(
+     videoURL: URL,
+     startPoint: Double,
+     endPoint: Double,
+     tiltList: [TimeStampedTilt] = [],
+     heightList: [TimeStampedHeight] = []
+ ) {
+     let _ = SwiftDataManager.shared.createClip(
+         videoURL: videoURL,
+         startPoint: startPoint,
+         endPoint: endPoint,
+         tiltList: tiltList,
+         heightList: heightList
+     )
+     SwiftDataManager.shared.saveContext()
+     loadClips()
+ }
+ 
+ func deleteClip(_ clip: Clip) {
+     SwiftDataManager.shared.deleteClip(clip)
+     SwiftDataManager.shared.saveContext()
+     loadClips()
+ }
+ ```
+ */
+
 @MainActor
 class SwiftDataManager {
     static let shared = SwiftDataManager()
@@ -30,6 +69,7 @@ class SwiftDataManager {
 
     // MARK: - Project
     
+    /// `Project` 생성
     func createProject(guide: Guide? = nil, clips: [Clip] = []) -> Project {
         let newProject = Project(guide: guide, clipList: clips)
         context.insert(newProject)
@@ -40,12 +80,14 @@ class SwiftDataManager {
     //    func fetchAllProjects() -> [Project] {
     //    }
 
+    /// `Project` 삭제
     func deleteProject(_ project: Project) {
         context.delete(project)
     }
 
     // MARK: - Clip
 
+    /// `Clip` 생성
     func createClip(
         videoURL: URL,
         startPoint: Double = 0,
@@ -64,18 +106,21 @@ class SwiftDataManager {
         return clip
     }
 
+    /// `Clip` 가져오기
     func fetchClip(byID id: String) -> Clip? {
         let predicate = #Predicate<Clip> { $0.id == id }
         let descriptor = FetchDescriptor<Clip>(predicate: predicate)
         return try? context.fetch(descriptor).first
     }
 
+    /// `Clip` 삭제하기
     func deleteClip(_ clip: Clip) {
         context.delete(clip)
     }
 
     // MARK: - Guide
 
+    /// `Guide` 생성
     func createGuide(
         clipID: String,
         bBoxPosition: CGPoint,
@@ -96,18 +141,21 @@ class SwiftDataManager {
         return guide
     }
 
+    /// `Guide` 가져오기
     func fetchGuide(forClipID clipID: String) -> Guide? {
         let predicate = #Predicate<Guide> { $0.clipID == clipID }
         let descriptor = FetchDescriptor<Guide>(predicate: predicate)
         return try? context.fetch(descriptor).first
     }
 
+    /// `Guide` 삭제하기
     func deleteGuide(_ guide: Guide) {
         context.delete(guide)
     }
 
     // MARK: - Save & Rollback
 
+    /// Context 저장하기 - 변경사항 반영
     func saveContext() {
         do {
             try context.save()
