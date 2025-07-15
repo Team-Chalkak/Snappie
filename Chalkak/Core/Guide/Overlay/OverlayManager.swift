baa71dc3342ac9060139f19ca4f7b5c84a3cdfe1
pearhyunjin <pearhyunjin@gmail.com>
2025-07-16 00:44:55 +0900

    ✨ [feat] #14 프레임에서 바운딩 박스 추출해 가이드 만들기

--- Chalkak/Core/Guide/Overlay/OverlayManager.swift
+++ Chalkak/Core/Guide/Overlay/OverlayManager.swift
@@ -36,9 +36,12 @@ class OverlayManager: ObservableObject {
                 try handler.perform([rectangleRequest, maskRequest])
 
                 // 2단계: BoundingBox 추출
-                if let result = rectangleRequest.results?.first as? VNHumanObservation {
+                if let results = rectangleRequest.results as? [VNHumanObservation], !results.isEmpty {
+                    let boxes = results.map { $0.boundingBox }
+                    let averageBox = boxes.average() // ⬅️ 이미 ViewModel에서 사용하던 확장 활용
+
                     DispatchQueue.main.async {
-                        self.boundingBox = result.boundingBox
+                        self.boundingBox = averageBox
                     }
                 }
 
