7db68f8bdd4802d3632f3076800cf06059593d13
finn <bishoe01@gmail.com>
2025-07-17 10:40:40 +0900

    🐛 [fix] guidoutlineImage 인터렉션 방지 로직 추가

--- Chalkak/Presentation/Guide/Distance/BoundingBoxView.swift
+++ Chalkak/Presentation/Guide/Distance/BoundingBoxView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct BoundingBoxView: View {
     let guide: Guide?
     let isFirstShoot: Bool
-    
+
     @StateObject private var viewModel = BoundingBoxViewModel()
     @StateObject private var cameraViewModel = CameraViewModel()
 
@@ -24,28 +24,30 @@ struct BoundingBoxView: View {
                         .ignoresSafeArea()
                         .transition(.opacity)
                 }
-                
+
                 CameraView(isFirstShoot: isFirstShoot, guide: guide, viewModel: cameraViewModel)
                     .onAppear {
                         cameraViewModel.setBoundingBoxUpdateHandler { bboxes in
                             viewModel.liveBoundingBoxes = bboxes
                         }
                     }
-                
+
                 if let guide = guide, let outline = guide.outlineImage {
                     Image(uiImage: outline)
                         .resizable()
                         .aspectRatio(contentMode: .fit)
                         .frame(width: 296, height: 526)
+                        .allowsHitTesting(false)
                 } else {
                     Text("윤곽선 이미지 없음")
                         .foregroundColor(.gray)
+                        .allowsHitTesting(false)
                 }
-                
+
                 // TODO: - Height, Tilt 피드백 뷰 띄우기
             }
         }
-        .onAppear() {
+        .onAppear {
             if let guide = guide {
                 viewModel.setReference(from: guide)
             }
