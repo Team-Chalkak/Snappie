02165294cd9cc89c716e3e68126653e00fdc9b56
finn <bishoe01@gmail.com>
2025-07-25 00:53:43 +0900

    💄 [style] #90 recordButton 컴포넌트화

--- Chalkak/Presentation/Camera/SubViews/CameraRecordView.swift
+++ Chalkak/Presentation/Camera/SubViews/CameraRecordView.swift
@@ -17,38 +17,30 @@ struct CameraRecordView: View {
             
             Spacer()
             
-            Button(action: {
+            RecordButton(
+                isRecording: viewModel.isRecording,
+                isTimerRunning: viewModel.isTimerRunning
+            ) {
                 if viewModel.isRecording || viewModel.isTimerRunning {
                     viewModel.stopVideoRecording()
                 } else {
                     viewModel.startVideoRecording()
                 }
-            }) {
-                ZStack {
-                    Circle()
-                        .stroke(lineWidth: 3)
-                        .frame(width: 74, height: 74)
-                        .foregroundColor(.white)
-                    
-                    if viewModel.isTimerRunning {
-                        Image(systemName: "xmark")
-                            .font(.system(size: 24, weight: .bold))
-                            .foregroundColor(.white)
-                    } else if !viewModel.isRecording {
-                        Circle()
-                            .fill(.white)
-                            .frame(width: 62, height: 62)
-                    } else {
-                        RoundedRectangle(cornerRadius: 10)
-                            .fill(.red)
-                            .frame(width: 26, height: 26)
-                    }
-                }
             }
             
             Spacer()
             
-            CircleIconButton(iconName: "arrow.trianglehead.2.clockwise.rotate.90", action: viewModel.changeCamera)
+            Button(action: viewModel.changeCamera) {
+                Image(Icon.conversion.rawValue)
+                    .font(.system(size: 20))
+            }
+            .padding(.horizontal, 18)
+            .padding(.vertical, 6)
+            .frame(width: 56, height: 32)
+            .background(
+                Capsule()
+                    .fill(SnappieColor.containerFillNormal)
+            )
         }
         .padding(.bottom, 20)
         .padding(.horizontal, 15)
