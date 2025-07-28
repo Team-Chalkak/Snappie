421473f60e9b3a89023bbb59c406840eac96290a
SSOL <msseock@gmail.com>
2025-07-28 21:44:55 +0900

    ✨ [feat] #134 리스트용 컴포넌트 구현
    - 프로젝트 리스트용 뷰 컴포넌트를 서브뷰로 나눠서 구현했습니다.
    - 더미 뷰모델을 생성했습니다(여기서 작업하실 수 있도록)

---
+++ Chalkak/Presentation/ProjectList/ProjectListViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  ProjectListViewModel.swift
+//  Chalkak
+//
+//  Created by 석민솔 on 7/28/25.
+//
+
+import Foundation
+
+final class ProjectListViewModel: ObservableObject {
+    
+}
---
+++ Chalkak/Presentation/ProjectList/SubViews/ProjectCard/ProjectInfoTextView.swift
@@ -0,0 +1,70 @@
+//
+//  ProjectInfoTextView.swift
+//  Chalkak
+//
+//  Created by 석민솔 on 7/28/25.
+//
+
+import SwiftUI
+
+/// 프로젝트 제목, 날짜를 보여주는 텍스트 위주의 뷰
+struct ProjectInfoTextView: View {
+    // MARK: input properties
+    /// 촬영중인 프로젝트면 disable하기위한 Bool 변수
+    let isCurrentProject: Bool
+    
+    /// 제목
+    let projectTitle: String
+    /// 조회가 됐던 프로젝트인지 확인용 변수
+    let isSeen: Bool
+    /// 날짜
+    let timeCreated: Date
+    
+    // MARK: computed proeprties
+    var timeCreatedString: String {
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "ko_KR")
+        formatter.dateFormat = "yyyy.MM.dd"
+        return formatter.string(from: timeCreated)
+    }
+    
+    // MARK: body
+    var body: some View {
+        VStack(alignment: .leading, spacing: 6) {
+            HStack(alignment: .center, spacing: 2) {
+                // 조회가 되면 없어지는 빨간 원
+                if !isCurrentProject && isSeen {
+                    Circle()
+                        .frame(width: 8, height: 8)
+                        .foregroundStyle(Color.redRecording)
+                }
+                
+                // 제목
+                Text(projectTitle)
+                    .font(SnappieFont.style(.proLabel2))
+                    .foregroundStyle(
+                        isCurrentProject
+                        ? SnappieColor.labelPrimaryDisable
+                        : SnappieColor.labelPrimaryNormal
+                    )
+            }
+            // 날짜
+            Text(timeCreatedString)
+                .font(SnappieFont.style(.proCaption1))
+                .foregroundStyle(SnappieColor.labelDarkInactive)
+        }
+    }
+}
+
+#Preview {
+    ProjectInfoTextView(
+        isCurrentProject: true,
+        projectTitle: "Project title",
+        isSeen: true,
+        timeCreated: Date()
+    )
+    .frame(width: 300, height: 300)
+    .background(
+        Color.black
+    )
+}
