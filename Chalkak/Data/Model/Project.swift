9bdf685325f07acaa0d96d0067d060cca841a5bf
YouBin <alicee0047@gmail.com>
2025-07-29 00:40:51 +0900

    ✨ [feat] project 모델 변수 추가 - coverImage, title

--- Chalkak/Data/Model/Project.swift
+++ Chalkak/Data/Model/Project.swift
@@ -26,13 +26,19 @@ class Project: Identifiable {
     /// 프로젝트가 삭제되면 함께 삭제됩니다.
     @Relationship(deleteRule: .cascade) var cameraSetting: CameraSetting?
     
+    /// 프로젝트 제목
+    var title: String
+    
     /// 첫번째 클립의 트리밍된 클립 길이.
     /// 이후 클립 편집 시 초기값으로 사용.
     var referenceDuration: Double?
     
     /// 유저가 새 프로젝트를 확인했는지 여부
     var isChecked: Bool = false
-
+    
+    /// 프로젝트 커버 이미지 - 첫번째 영상 첫번째 프레임으로 설정
+    var coverImage: Data?
+    
     /// 새로운 `Project` 인스턴스를 초기화합니다.
     /// - Parameters:
     ///   - id: 고유 식별자 (기본값은 자동 생성된 UUID).
@@ -43,14 +49,18 @@ class Project: Identifiable {
         guide: Guide? = nil,
         clipList: [Clip] = [],
         cameraSetting: CameraSetting? = nil,
+        title: String = "",
         referenceDuration: Double? = nil,
-        isChecked: Bool = true
+        isChecked: Bool = true,
+        coverImage: Data? = nil
     ) {
         self.id = id
         self.guide = guide
         self.clipList = clipList
         self.cameraSetting = cameraSetting
+        self.title = title
         self.referenceDuration = referenceDuration
         self.isChecked = isChecked
+        self.coverImage = coverImage
     }
 }
