a7145f24bc991a3d10c9c229a40001227d3b1697
finn <bishoe01@gmail.com>
2025-12-15 22:12:43 +0900

    💄 [feat]  #16 버튼 디자인 세부 디테일 조정

--- Chalkak/Presentation/Camera/SubViews/CameraRecordView.swift
+++ Chalkak/Presentation/Camera/SubViews/CameraRecordView.swift
@@ -15,13 +15,11 @@ struct CameraRecordView: View {
 
     var body: some View {
         HStack(spacing: 0) {
-            Button(action: {
-                coordinator.push(.projectList)
-                Analytics.logEvent("moveToProjectListButtonTapped", parameters: nil)
-            }) {
-                Image(viewModel.hasBadge ? "projectListBadge" : "projectList")
-                    .frame(width: 48, height: 48)
-            }
+            Button {}
+                label: {
+                    Color.clear
+                        .frame(width: 48, height: 48)
+                }
 
             Spacer()
 
