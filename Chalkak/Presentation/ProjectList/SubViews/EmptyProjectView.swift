8c855427c4eaec969b7c7178894a5c6cd1182084
finn <bishoe01@gmail.com>
2025-12-26 18:56:57 +0900

    💄 [style] ProjectListView가 비어있을때에 대한 UI 조정

--- Chalkak/Presentation/ProjectList/SubViews/EmptyProjectView.swift
+++ Chalkak/Presentation/ProjectList/SubViews/EmptyProjectView.swift
@@ -10,7 +10,12 @@ import SwiftUI
 /// 프로젝트가 비어있을 때의 안내 뷰
 struct EmptyProjectView: View {
     var body: some View {
-        VStack {
+        VStack(spacing: 0) {
+            Text("나의 프로젝트")
+                .font(.system(size: 22, weight: .bold))
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.bottom, 32)
+
             Spacer()
 
             Text("프로젝트가 비어 있어요.\n촬영을 시작하여 새 프로젝트를 만들어볼까요?")
